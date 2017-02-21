a734fa9784dc22cb34c53ff0a445fb92bf637101
liaobaocheng <liaobaocheng90@gmail.com>
2017-02-21 15:42:48 +0800

    1.开始项目

---
+++ resources/lang/zh/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => '密码必须是6个字符以上',
+    'reset' => '你的密码已经重置',
+    'sent' => '密码重置链接已发送到你的邮箱',
+    'token' => '密码重置口令已失效',
+    'user' => "没有找到匹配该邮箱的用户",
+
+];
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,11 +6,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
         <style>
             html, body {
                 background-color: #fff;
@@ -80,14 +75,6 @@
             <div class="content">
                 <div class="title m-b-md">
                     Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
             </div>
         </div>
