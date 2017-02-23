d90bc2d019c6d215ff55100f7b7de39b03977cbd
liaobaocheng <liaobaocheng90@gmail.com>
2017-02-23 09:48:56 +0800

    5.集成富文本和图片上传

--- resources/views/backend/news/create.blade.php
+++ resources/views/backend/news/create.blade.php
@@ -16,7 +16,8 @@
 
         <div class="form-group">
             {{ Form::label('content', '新闻内容') }}
-            <textarea name="content" class="form-control"></textarea>
+            <textarea name="content" class="form-control my-editor"></textarea>
+
         </div>
 
         {{ Form::submit('保存新闻', array('class' => 'btn btn-primary')) }}
@@ -25,15 +26,41 @@
 @endsection
 
 @section('scripts')
-    <script type="text/javascript" src="/vendor/wangeditor/js/lib/jquery-1.10.2.min.js"></script>
-    <script type="text/javascript" src="/vendor/wangeditor/js/wangEditor.js"></script>
+    <script src="{{ asset('js/tinymce/tinymce.min.js')}}"></script>
     <script>
-        var textarea = $('textarea');
-        var editor = new wangEditor(textarea);
-        editor.config.menus = [
-            'underline','bold','undo', 'redo','link', 'img','fullscreen'
-        ];
-        editor.config.hideLinkImg = true;
-        editor.create();
+        var editor_config = {
+            path_absolute : "/",
+            selector: "textarea.my-editor",
+            language: 'zh_CN',
+            plugins: [
+                "advlist autolink lists link image charmap print preview hr anchor pagebreak",
+                "searchreplace wordcount visualblocks visualchars code fullscreen",
+                "insertdatetime media nonbreaking save table contextmenu directionality",
+                "emoticons template paste textcolor colorpicker textpattern"
+            ],
+            toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
+            relative_urls: false,
+            file_browser_callback : function(field_name, url, type, win) {
+                var x = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth;
+                var y = window.innerHeight|| document.documentElement.clientHeight|| document.getElementsByTagName('body')[0].clientHeight;
+
+                var cmsURL = editor_config.path_absolute + 'laravel-filemanager?field_name=' + field_name;
+                if (type == 'image') {
+                    cmsURL = cmsURL + "&type=Images";
+                } else {
+                    cmsURL = cmsURL + "&type=Files";
+                }
+
+                tinyMCE.activeEditor.windowManager.open({
+                    file : cmsURL,
+                    title : '图片管理器',
+                    width : x * 0.8,
+                    height : y * 0.8,
+                    resizable : "yes",
+                    close_previous : "yes"
+                });
+            }
+        };
+        tinymce.init(editor_config);
     </script>
 @endsection
